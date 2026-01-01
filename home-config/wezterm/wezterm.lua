51c6ecd27936a75000ce74411e73c2b344ced544
binwan-dev <email@wanbin.tech>
2026-01-01 16:42:44 +0800

    feat: change theme and fonts

--- home-config/wezterm/wezterm.lua
+++ home-config/wezterm/wezterm.lua
@@ -1,13 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 -- font
 config.font_size = 14
+config.font = wezterm.font("Maple Mono Normal NF CN", { weight = "Medium", stretch = "Normal", style = "Normal" })
 
 -- colors
 local appearance_themes = {
-  Light = 'Atom One Light',
-  Dark = 'Doom One',
+	Light = "Atom One Light",
+	Dark = "Doom One",
 }
 config.color_scheme = appearance_themes[wezterm.gui.get_appearance()] or dark_theme
 
@@ -15,63 +16,61 @@ config.color_scheme = appearance_themes[wezterm.gui.get_appearance()] or dark_th
 
 -- keybindings
 config.keys = {
-    -- 禁用 Ctrl+Shift+C (复制)
-    {
-      key = 'p',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SendKey {
-        key = 'p',
-        mods = 'CTRL|ALT',
-      },
-    },
-    -- 禁用 Ctrl+Shift+N (新窗口)
-    {
-      key = 'n',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SendKey {
-        key = 'n',
-        mods = 'CTRL|ALT',
-      },
-    },
-    -- 禁用 Ctrl+Shift+W (关闭窗口)
-    {
-      key = 'f',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SendKey {
-        key = 'f',
-        mods = 'CTRL|ALT',
-      },
-    },
-    -- 禁用 Ctrl+Shift+W (关闭窗口)
-    {
-      key = 'b',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SendKey {
-        key = 'b',
-        mods = 'CTRL|ALT',
-      },
-    },
-    -- 禁用 Ctrl+Shift+W (关闭窗口)
-    {
-      key = 'a',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SendKey {
-        key = 'a',
-        mods = 'CTRL|ALT',
-      },
-    },
-    -- 禁用 Ctrl+Shift+W (关闭窗口)
-    {
-      key = 'e',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.SendKey {
-        key = 'e',
-        mods = 'CTRL|ALT',
-      },
-    },
-  }
-
-
+	-- 禁用 Ctrl+Shift+C (复制)
+	{
+		key = "p",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SendKey({
+			key = "p",
+			mods = "CTRL|ALT",
+		}),
+	},
+	-- 禁用 Ctrl+Shift+N (新窗口)
+	{
+		key = "n",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SendKey({
+			key = "n",
+			mods = "CTRL|ALT",
+		}),
+	},
+	-- 禁用 Ctrl+Shift+W (关闭窗口)
+	{
+		key = "f",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SendKey({
+			key = "f",
+			mods = "CTRL|ALT",
+		}),
+	},
+	-- 禁用 Ctrl+Shift+W (关闭窗口)
+	{
+		key = "b",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SendKey({
+			key = "b",
+			mods = "CTRL|ALT",
+		}),
+	},
+	-- 禁用 Ctrl+Shift+W (关闭窗口)
+	{
+		key = "a",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SendKey({
+			key = "a",
+			mods = "CTRL|ALT",
+		}),
+	},
+	-- 禁用 Ctrl+Shift+W (关闭窗口)
+	{
+		key = "e",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SendKey({
+			key = "e",
+			mods = "CTRL|ALT",
+		}),
+	},
+}
 
 -- config.disable_default_key_bindings = true
 return config
